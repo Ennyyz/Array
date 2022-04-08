f376feb3720fc613004bcf0a86954911e19d2dc3
LAPTOP-7M6VPU5A\ASUS <ngocyen2812.pham@gmail.com>
2022-04-08 16:04:24 +0700

    tét

---
+++ Slide12.Buổi1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slide12.List
+{
+    class Program
+    {
+        /*
+         Bài tập: Viết chương trình thực hiện các yêu cầu sau:
+            a. Khai báo và khởi tạo một List số nguyên. In ra màn hình giá trị thuộc tính Count và Capacity của List
+            b. Thêm lần lượt các số nguyên từ 1 đến 10 vào cuối List. Sau mỗi lượt thêm in ra màn hình giá trị các phần tử hiện có trong List, các thuộc tính Count và Capacity của List
+            c. Lần lượt xóa phần tử ở đầu List cho đến khi hết danh sách. Sau mỗi lượt xóa in ra màn hình giá trị các phần tử hiện có trong List, các thuộc tính Count và Capacity của List
+         */
+        static void nhaplist(out List<int> list)
+        {
+            list = new List<int>();
+            Console.WriteLine($"thuoc tinh count cua list la: {list.Count}");
+            Console.WriteLine($"thuoc tinh capacity cua list la:{list.Capacity}");
+        }
+        static void nhapphantu(List<int> list)
+        {
+            int value = 1;
+            for (int i = 0;i<10;i++)
+            {
+                list.Add(value);
+                value++;
+                foreach (var item in list)
+                    Console.Write($"{item,3}");
+                Console.WriteLine($"\nCount : {list.Count}");
+                Console.WriteLine($"Capacity: {list.Capacity}");
+            }
+        }
+        static void xoaphantu(List<int>list)
+        {
+            while(list.Count > 0)
+            { 
+                list.RemoveAt(0);
+                foreach (var item in list)
+                    Console.Write($"{item,3}");
+                Console.WriteLine($"\n count : {list.Count}");
+                Console.WriteLine($"capacity: {list.Capacity}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> list;
+            nhaplist(out list);
+            nhapphantu(list);
+            xoaphantu(list);
+        }
+    }
+}
---
+++ Slide7.Buổi1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SLIDE7.MANG1CHIEU.BAI1.MANG2CHIEU
+{
+    class Program
+    {
+        /*
+         Bài tập: Viết chương trình có các hàm chức năng sau:
+            a. Hàm nhập một mảng số nguyên (bao gồm các thao tác: nhập số lượng phần tử, khởi tạo và nhập giá trị các phần tử của mảng)
+            b. Hàm nhập giá trị các phần tử của một mảng số nguyên (mảng đã được khởi tạo trước khi gọi hàm)
+            c. Hàm xuất giá trị các phần tử của một mảng số nguyên ra màn hình
+         */
+        static void nhapmangsonguyen(out int[] arr)
+        {
+            Console.Write("nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+            arr= new int[n];
+        }
+        static int[] nhapmangsonguyen1()
+        {
+            Console.Write("Nhap n: "); int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            return a;
+        }
+        static void nhapmang(int[]a)
+        {
+            for(int i = 0;i < a.Length;i++)
+            {
+                Console.Write($"a[{i}]: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void xuatmang(int[]a)
+        {
+            //foreach(int item in a)
+            foreach (var item in a)
+            {
+                Console.Write($"{item,3}");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] a;
+            nhapmangsonguyen(out a);
+            nhapmang(a);
+            xuatmang(a);
+
+            Console.ReadLine();
+        }
+    }
+}
